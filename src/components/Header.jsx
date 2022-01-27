134c4828c23a3cd51e49c72b8e1392edb05a36ad
DanielDdungu <daddungu@outlook.com>
2022-01-27 09:13:56 +0100

    Chat messages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Header() {
     return (
         <>
-            <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box place-content-center fixed top-0 left-0 right-0">
-                <div className="px-2 mx-2 navbar-center ">
-                    <span className="text-lg font-bold">Sorting Hat</span>
+            <div className="relative">
+                <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box place-content-center fixed top-0 left-0 right-0">
+                    <div className="px-2 mx-2 navbar-center ">
+                        <span className="text-lg font-bold">Sorting Hat</span>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -9,7 +9,7 @@ function Questions() {
                 <div className="grid grid-cols-4 gap-4 justify-between place-content-center h-32 ... bg-base-200 drop-shadow-2xl fixed top-24 left-0 right-0">
                     <div className=" grid place-items-center">
                         <div className="avatar ">
-                            <div className="mb-8 rounded-box w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <div className="mb-8 rounded-box w-24 h-24  ring ring-primary ring-offset-base-100 ring-offset-2">
                                 <img
                                     src="http://daisyui.com/tailwind-css-component-profile-1@94w.png"
                                     alt=""
@@ -50,24 +50,64 @@ function Questions() {
                 </div>
 
                 {/**Question Section*/}
-                <div className=" px-12 overflow-y-scroll absolute inset-x-0 bottom-0 h-5 ">
-                    <div className="alert alert-success ">
+                <div className=" px-12 overflow-y-auto   fixed top-60 left-0 right-0 h-64 ">
+                    <div className="alert alert-success  my-3 rounded-tr-none">
                         <div className="flex-1">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                className="w-6 h-6 mx-2 stroke-current"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                                ></path>
-                            </svg>
                             <label>
-                                Lorem ipsum dolor sit amet, consectetur adip!
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book.
+                            </label>
+                        </div>
+                    </div>
+                    <div className="alert alert-success my-3 rounded-tr-none">
+                        <div className="flex-1">
+                            <label>
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book.
+                            </label>
+                        </div>
+                    </div>
+                    <div className="alert alert-success  rounded-tr-none">
+                        <div className="flex-1">
+                            <label>
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book.
+                            </label>
+                        </div>
+                    </div>
+                    <div className="alert alert-success my-3 rounded-tr-none">
+                        <div className="flex-1">
+                            <label>
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book.
+                            </label>
+                        </div>
+                    </div>
+                    <div className="alert alert-success my-3 rounded-tr-none">
+                        <div className="flex-1">
+                            <label>
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book.
                             </label>
                         </div>
                     </div>
